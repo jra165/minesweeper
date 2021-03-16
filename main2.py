72a98c33156120945190dc649280e422a1a5be1c
jra165 <36318088+jra165@users.noreply.github.com>
2021-03-16 12:45:57 -0400

    initial commit

---
+++ main2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 16 12:42:05 2021
+
+@author: Joshua Atienza, Janet Zhang
+@net-id: jra165, jrz42
+@project: Minesweeper
+"""
+
