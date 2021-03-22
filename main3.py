c2993a671bb4ad506fbe50df2f70317b1494c5a9
jra165 <36318088+jra165@users.noreply.github.com>
2021-03-21 21:58:02 -0400

    working code

--- main3.py
+++ main3.py
@@ -170,18 +170,18 @@ Code for our basic agent
 def simpleAgent(field, dim):
 
     # The Agent's information about the environment
-    minesMarked=[]
-    minesExploded=[]
-    cellsSafe=[]
-    cellsHidden=[]
-    cellsExplored=[]
+    minesMarked = []
+    minesExploded = []
+    cellsSafe = []
+    cellsHidden = []
+    cellsExplored = []
 
     # Initialize the Agent's knowledge
     for x in range(len(field)):
         cellsHidden.append(field[x].index)
 
     # Explore the entire environment
-    while(len(cellsHidden)!=0):     
+    while(len(cellsHidden)!=0 or len(cellsSafe) != 0):     
 
         # If there are known safe moves, explore those first
         if(len(cellsSafe) > 0):
@@ -214,7 +214,9 @@ def simpleAgent(field, dim):
                     if((agentCell.neighborsHidden[x] not in minesMarked)):
                         cellsHidden.remove(agentCell.neighborsHidden[x])
                         minesMarked.append(agentCell.neighborsHidden[x])
-                    minedetect(field,field[agentCell.neighborsHidden[x]])
+                    node = field[agentCell.neighborsHidden[x]]
+                    for i in range(len(node.neighborsAll)):
+                        field[node.neighborsAll[i]].agentsMines += 1
                     for y in range(len(field)):
                         if agentCell.neighborsHidden[x] in field[y].neighborsHidden and y!=agentCell.index:
                             field[y].neighborsHidden.remove(agentCell.neighborsHidden[x])
@@ -253,16 +255,138 @@ def simpleAgent(field, dim):
     return len(minesMarked)
 
 
-def minedetect(field,node):
-    for x in range(len(node.neighborsAll)):
-        field[node.neighborsAll[x]].agentsMines += 1
-def tilesolved(field,node):
-    if(len(field[node].neighborsAll)-field[node].clue-len(field[node].neighborsSafe)==len(field[node].neighborsHidden)):
-        return True
-    elif(field[node].clue - field[node].agentsMines == len(field[node].neighborsHidden)):
-        return True
-    else:
-        return False
+"""
+Code for advanced agent
+"""
+def advAgent(field, dim):
+    
+    # The Agent's information about the environment
+    minesMarked=[]
+    minesExploded=[]
+    cellsSafe=[]
+    cellsHidden=[]
+    cellsExplored=[]
+    isDone = [False] * len(field)
+    
+    # Initialize the Agent's knowledge
+    for x in range(len(field)):
+        cellsHidden.append(field[x].index)
+    
+    
+    # Explore the entire environment
+    while(len(cellsHidden) != 0 or len(cellsSafe) != 0):
+        
+        if(len(cellsSafe) == 0):                            # path if no more safe nodes
+            ranindex=random.randrange(len(cellsHidden))      
+            agentCell=field[cellsHidden[ranindex]]            
+            cellsHidden.pop(ranindex)
+            
+            
+        elif(len(cellsSafe) > 0):                            
+
+            ranindex=cellsSafe.pop(0)                        
+            agentCell=field[ranindex]     
+
+            #If chosen cell in Hidden, remove                    
+            if(ranindex in cellsHidden):
+                cellsHidden.remove(ranindex)               
+                
+        #If no safe moves, make a random move        
+
+        
+        
+        # If random move was a mine, update Agent knowledge
+        if(agentCell.isMine == True):                    # path if node selected from random is a mine
+            minesExploded.append(agentCell.index)             
+            for x in range(len(agentCell.neighborsAll)):
+                field[agentCell.neighborsAll[x]].agentsMines += 1
+                
+                """added if statement"""
+                if(agentCell.index in field[agentCell.neighborsAll[x]].neighborsHidden):
+                    field[agentCell.neighborsAll[x]].neighborsHidden.remove(agentCell.index)
+            
+                    
+        # If random move was not a mine, check for the conditions that the Simple Agent recognizes
+        else:                 # path if node selected is not a mine
+        
+            cellsExplored.append(agentCell.index)
+            
+            #  If, for a given cell, the total number of mines (the clue) minus the number of revealed mines is the number of
+            #  hidden neighbors, every hidden neighbor is a mine.
+            
+            if(agentCell.clue - agentCell.agentsMines == len(agentCell.neighborsHidden)): # every node is a mine around it
+                for x in range(len(agentCell.neighborsHidden)):   # for all hidden neighbors
+                  #  print(agentCell.neighborsHidden)
+                    if((agentCell.neighborsHidden[x] not in minesMarked)):    # if the mine is not ID'd
+                        cellsHidden.remove(agentCell.neighborsHidden[x])   # remove mine from cellsHidden
+                        minesMarked.append(agentCell.neighborsHidden[x])      # append mine to ID mines
+                        isDone[agentCell.neighborsHidden[x]]=True
+                        
+                    node = field[agentCell.neighborsHidden[x]]
+                    for i in range(len(node.neighborsAll)):
+                        field[node.neighborsAll[i]].agentsMines += 1
+                    for y in range(len(field)):                 # for all nodes in the field - remove agentCell node from hidden node list of each node
+                        if agentCell.neighborsHidden[x] in field[y].neighborsHidden and y!=agentCell.index:
+                            field[y].neighborsHidden.remove(agentCell.neighborsHidden[x])
+                            
+                agentCell.neighborsHidden.clear() # no more hidden nodes in agentCell
+                
+            elif(len(agentCell.neighborsAll)-agentCell.clue-len(agentCell.neighborsSafe)==len(agentCell.neighborsHidden)): # every node is safe around it
+                for x in range(len(agentCell.neighborsAll)):       # for all hidden neighbors
+                    if(agentCell.neighborsAll[x] not in cellsSafe and (not isDone[agentCell.neighborsAll[x]])):         # if neighbor index is not in safe
+                        cellsSafe.append(agentCell.neighborsAll[x])         # add neighbor index to safe
+                agentCell.neighborsHidden.clear()                     # remove all neighbors from node's hidden
+            for x in range(len(agentCell.neighborsAll)):        # for all neighbors
+                if (agentCell not in field[agentCell.neighborsAll[x]].neighborsSafe) :
+                    field[agentCell.neighborsAll[x]].neighborsSafe.append(agentCell)   # append original node to all neighbors safe spaces
+                if len(field[agentCell.neighborsAll[x]].neighborsHidden) != 0:           # for all neighbors, remove original node from their hidden
+                    if(agentCell.index in field[agentCell.neighborsAll[x]].neighborsHidden):
+                        field[agentCell.neighborsAll[x]].neighborsHidden.remove(agentCell.index)
+        
+
+        # Run only if we have extracted all information from a given cell
+        if(len(agentCell.neighborsAll) - agentCell.agentsMines - len(agentCell.neighborsSafe) == 0 or agentCell.isMine):
+            
+            temp = agentCell.index
+            if(temp in cellsExplored):
+                cellsExplored.remove(temp)
+            if(temp in cellsSafe):
+                cellsSafe.remove(temp)
+            isDone[temp] = True
+            
+            # Add explored cells to safe, eliminating duplicates with set
+            cellsSafe = list(set().union(cellsExplored, cellsSafe))
+            cellsExplored.clear()
+        
+        #Run if we do not know all information about a given cell
+        else:
+            
+            temp = []
+            for x in range(len(cellsExplored)):
+                
+                cell = cellsExplored[x]
+                if (len(field[cell].neighborsAll)-field[cell].clue-len(field[cell].neighborsSafe)==len(field[cell].neighborsHidden) 
+                    or field[cell].clue - field[cell].agentsMines == len(field[cell].neighborsHidden)):
+                    temp.append(cell)
+                    cellsSafe.append(cell)
+        
+            for y in range(len(temp)):
+                cellsExplored.remove(temp[y])
+                
+        print("Unsearched " +str(cellsHidden))
+        print("safe " + str(cellsSafe))
+        print("IdedMines "+str(minesMarked))
+        print("Exploded mines " + str(minesExploded))
+        print("Explored "+str(cellsExplored) + " " + str(len(cellsExplored)))
+        print("Finished "+str(isDone))
+        print("\n \n")
+        print("current status")
+        print_field(field, dim)
+        
+        
+    return len(minesMarked)
+
+        
 
 def main():
 
@@ -279,6 +403,8 @@ def main():
         mazefieldCopy = copy.deepcopy(mazefield)
         mazefieldMineCount=copy.deepcopy(mazefield)
         minesdetected=simpleAgent(mazefield,dim)
+        #minesdetected2=advAgent(mazefieldCopy,dim)
+        #advanced.append(minesdetected2)
 
 if __name__ == "__main__":
     main()
